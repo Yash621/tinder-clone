460044eb90217fefdbc4e2b58e8e8034302c9597
yash621 <yg17381@gmail.com>
2021-06-04 23:35:58 +0530

    random changes

--- src/Header.js
+++ src/Header.js
@@ -10,11 +10,17 @@ function Header() {
       <IconButton>
         <PersonIcon fontSize="large" className="header_icon" />
       </IconButton>
-      <img
-        className="header_logo"
-        src="https://globaldatinginsights.com/wp-content/uploads/2017/08/Screen-Shot-2017-08-17-at-10.04.531.png"
-        alt=""
-      />
+      <div>
+        <img
+          className="header_logo"
+          src="https://globaldatinginsights.com/wp-content/uploads/2017/08/Screen-Shot-2017-08-17-at-10.04.531.png"
+          alt=""
+        />
+        <center>
+          <h3>tinder</h3>
+        </center>
+      </div>
+
       <IconButton>
         <ForumIcon fontSize="large" className="header_icon" />
       </IconButton>
